6c4d8632143c83be3c64113eaa5c2e604e998f99
vagrant <vagrant@dev-cent7.box>
2018-12-21 21:34:19 +0000

    add some repos

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,10 @@
 
 # stuff we want
 yum_packages=(epel-release gcc tmux git python2-pip python-devel ncurses-devel ctags)
-python_packages=('ipython>=5,<6' requests pylint)
+python_packages=('ipython>=5,<6' requests pylint ipython)
+repos=(vagrant oren)
+
+github_key='235297140b5e3cfa028119e0e459e5d0e88042d0'
 
 for package in ${yum_packages[@]}; do
   yum install -y $package
@@ -10,6 +13,10 @@ done
 
 for package in ${python_packages[@]}; do
   pip install $package
+done
+
+for repo in ${repos[@]}; do
+  git clone "https://jonathanm:235297140b5e3cfa028119e0e459e5d0e88042d0@github.doubleverify.com/jonathanm/${repo}.git"
 done
 
 cp -R /files/. /home/vagrant
