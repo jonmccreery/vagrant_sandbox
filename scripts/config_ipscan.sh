90829a923cda72018e1a162d06ec6eb646516726
Jonathan McCreery <jonathan.mccreery@doubleverify.com>
2019-01-22 19:05:49 +0000

    first pass at automating the ip_scan box

--- scripts/config_ipscan.sh
+++ scripts/config_ipscan.sh
@@ -6,13 +6,10 @@ source /vagrant/scripts/util.sh
 yum_packages=(epel-release python34-pip gcc tmux htop tig git traceroute nc nmap python2-pip python-devel ncurses-devel ctags ShellCheck)
 python_packages=('ipython>=5,<6' requests flake8 pycodestyle pylint ipython virtualenv)
 python3_packages=(pylint)
-repos=(vagrant oren)
+repos=(Network)
 
 # TODO: THIS IS LIKE... REALLY REALLY INSECURE
 github_key='235297140b5e3cfa028119e0e459e5d0e88042d0'
-
-cp -R /files/dotfiles/.* /home/vagrant
-chown -R vagrant:vagrant /home/vagrant
 
 # create a build space
 mkdir /tmp/build
@@ -20,10 +17,9 @@ mkdir /tmp/build
 # main body
 install_packages
 clone_and_update_repos
-install_rbenv
-install_vim_8
-install_YouCompleteMe
 
 # clean up after ourselves
 rm -rf /tmp/build
 
+#####
+ln -s /repo/Network/util/netbox/ipcheck/ip_scan.py /usr/local/bin/ip_scan.py
