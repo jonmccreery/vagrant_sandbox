aefa2e73dca433716af3c07ed69afcb41b3f986a
Jonathan McCreery <jonathan.mccreery@doubleverify.com>
2019-01-29 20:17:13 +0000

    fix up dependencies

--- scripts/config_ipcheck.sh
+++ scripts/config_ipcheck.sh
@@ -4,7 +4,7 @@ source /vagrant/scripts/util.sh
 
 # stuff we want
 yum_packages=(epel-release python34-pip gcc tmux htop tig git traceroute nc nmap python2-pip python-devel)
-python_packages=('ipython>=5,<6' click netaddr python-nmap paramikao pynetbox)
+python_packages=('ipython>=5,<6' click netaddr ipdb python-nmap paramiko pynetbox pyyaml)
 repos=(Network)
 
 # TODO: THIS IS LIKE... REALLY REALLY INSECURE
